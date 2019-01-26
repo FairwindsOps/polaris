442b59d28a91b3b8760f6509d42ba103379fb04b
jessicagreben <Jessica.greben1+github@gmail.com>
2019-01-26 12:00:32 -0800

    remove dup get kubeconf

--- pkg/kube/clientset.go
+++ pkg/kube/clientset.go
@@ -2,60 +2,22 @@ package kube
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
 
-	homedir "github.com/mitchellh/go-homedir"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
 )
 
 func createClientset() *kubernetes.Clientset {
-	var err error
-	var config *rest.Config
-	kubeconfig := getKubeConfig()
+	kubeConf := config.GetConfigOrDie()
 
-	switch kubeconfig {
-	case "":
-		config, err = rest.InClusterConfig()
-	default:
-		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
-	}
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
+	clientset, err := kubernetes.NewForConfig(kubeConf)
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
 	return clientset
 }
 
-func getKubeConfig() string {
-	var path string
-
-	if os.Getenv("KUBECONFIG") != "" {
-		path = os.Getenv("KUBECONFIG")
-		return path
-	}
-
-	if home, err := homedir.Dir(); err == nil {
-		path = filepath.Join(home, ".kube", "config")
-	}
-
-	if _, err := os.Stat(path); err != nil {
-		// No kubeconfig exists, therefor return an emtpy string to
-		// indicate that this web server is running inside the cluster.
-		return ""
-	}
-	return path
-}
-
 var clientset = createClientset()
-
-// Kubernetes version 1.11 APIs
 
 // CoreV1API exports the v1 Core API client.
 var CoreV1API = clientset.CoreV1()
