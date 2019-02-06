535735fbde4d9002b298a80bc9b579a67efe007c
Rob Scott <rob.scott87@gmail.com>
2019-02-06 14:46:54 -0500

    fixes for linting, updating CI to fail when linting does

--- pkg/kube/clientset.go
+++ pkg/kube/clientset.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 
 	"k8s.io/client-go/kubernetes"
+	// Required for GKE auth
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 )
 
+// CreateClientset returns a new Kubernetes clientset
 func CreateClientset() *kubernetes.Clientset {
 	kubeConf := config.GetConfigOrDie()
 
