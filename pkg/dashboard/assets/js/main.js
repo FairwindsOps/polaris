807955d970ee75e25524e350f5cd23c98cea0b9a
Bobby Brennan <bobby.brennan@gmail.com>
2019-05-07 15:49:08 +0000

    fix expand toggling

--- pkg/dashboard/assets/js/main.js
+++ pkg/dashboard/assets/js/main.js
@@ -9,9 +9,7 @@ if (!Object.values) {
 
 $(function () {
   $('.card .resource-info .name').on('click', function (e) {
-    console.log('clicked', arguments)
-    console.log('parent', $(e.srcElement).parent('.resource-info'));
-    $(e.srcElement).parents('.resource-info').toggleClass('expanded');
+    $(this).parents('.resource-info').toggleClass('expanded');
   });
 
   var expandMatch = window.location.search.match(/expand=(\w+)(\W|$)/);
