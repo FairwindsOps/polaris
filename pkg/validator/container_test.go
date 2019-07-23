18b9f6db683b257cbf82f97ec1dff179578adfe5
Nick Huanca <nick@fairwinds.com>
2019-07-23 14:02:11 -0600

    Copyright rename

--- pkg/validator/container_test.go
+++ pkg/validator/container_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 ReactiveOps
+// Copyright 2019 Fairwinds
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
