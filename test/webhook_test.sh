9c172877ddd61ecdb344e0c6fe6de78c664dd0e4
Will Ledingham <wjl17@duke.edu>
2019-08-01 09:33:20 -0400

    code working now, clean up

--- test/webhook_test.sh
+++ test/webhook_test.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
-set -x
 sed -ri "s|'(quay.io/reactiveops/polaris:).+'|'\1${CIRCLE_SHA1}'|" ./deploy/webhook.yaml
-kubectl apply -f ./deploy/webhook.yaml
+kubectl apply &> /dev/null -f ./deploy/webhook.yaml
 sleep 20
-kubectl apply -f test/correctconfig.yaml
-status=$?
-sleep 20 
 
-
-if [ $status -eq 0 ]; then
+kubectl apply &> /dev/null -f test/correctconfig.yaml
+if [ $? -eq 0 ]; then
     VAR1="pass"
     echo pass 
 else
     echo "Test Failed: Polaris prevented a deployment with no configuration issues." 
 fi
 
-kubectl apply -f test/incorrectconfig.yaml
-status=$?
-sleep 20
-if [ $status -ne 0 ]; then
+kubectl apply &> /dev/null -f test/incorrectconfig.yaml
+if [ $? -ne 0 ]; then
     VAR2="pass"
     echo pass 
 else
