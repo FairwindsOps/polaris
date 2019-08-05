c51081472ce89dee6fdc7187784ce71b6870c137
Will Ledingham <wjl17@duke.edu>
2019-08-05 14:40:19 -0400

    ..

--- test/webhook_test.sh
+++ test/webhook_test.sh
@@ -10,6 +10,7 @@ timeout=100
 while kubectl apply -f test/failing_test.deployment.yaml &> /dev/null; do
   echo "Waiting for webhook to start..."
   if [ $timeout -eq 0 ]; then
+    kubectl apply -f test/failing_test.deployment.yaml
     kubectl get pods -n polaris
     echo "Timed out while waiting for webhook to start"
     exit 1
