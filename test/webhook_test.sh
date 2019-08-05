977253ed20ab4ff4e2bd76fc58252cd0de5042ce
Will Ledingham <wjl17@duke.edu>
2019-08-05 13:16:25 -0400

    timeout increase

--- test/webhook_test.sh
+++ test/webhook_test.sh
@@ -6,7 +6,7 @@ set -e
 sed -ri "s|'(quay.io/reactiveops/polaris:).+'|'\1${CIRCLE_SHA1}'|" ./deploy/webhook.yaml
 
 kubectl apply -f ./deploy/webhook.yaml &> /dev/null
-timeout=40
+timeout=100
 while kubectl apply -f test/failing_test.deployment.yaml &> /dev/null; do
   echo "Waiting for webhook to start..."
   if [ $timeout -eq 0 ]; then
