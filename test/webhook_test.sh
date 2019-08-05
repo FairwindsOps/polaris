9176ab6353515212556d8e1041af583077053688
Will Ledingham <wjl17@duke.edu>
2019-08-05 12:59:46 -0400

    .

--- test/webhook_test.sh
+++ test/webhook_test.sh
@@ -6,7 +6,7 @@ set -e
 sed -ri "s|'(quay.io/reactiveops/polaris:).+'|'\1${CIRCLE_SHA1}'|" ./deploy/webhook.yaml
 
 kubectl apply -f ./deploy/webhook.yaml &> /dev/null
-timeout=15
+timeout=40
 while kubectl apply -f test/failing_test.deployment.yaml &> /dev/null; do
   echo "Waiting for webhook to start..."
   if [ $timeout -eq 0 ]; then
@@ -15,8 +15,8 @@ while kubectl apply -f test/failing_test.deployment.yaml &> /dev/null; do
   fi
   timeout=$((timeout-1))
   sleep 1
-  kubectl delete nginx-deployment
 done
+kubectl delete nginx-deployment
 echo "Webhook started!"
 
 #Webhook started, setting all tests as passed initially.
