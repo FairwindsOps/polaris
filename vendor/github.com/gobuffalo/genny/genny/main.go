790607c6c00b675617671bb4af8c9d36350282d7
Robert Brennan <bobby.brennan@gmail.com>
2019-12-19 17:52:35 +0000

    remove vendor

--- vendor/github.com/gobuffalo/genny/genny/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/gobuffalo/genny/genny/cmd"
-
-func main() {
-	cmd.Execute()
-}
--- vendor/google.golang.org/grpc/.github/ISSUE_TEMPLATE/question.md
+++
@@ -1,6 +0,0 @@
----
-name: Question
-about: Ask a question about gRPC-Go
-labels: 'Type: Question'
-
----
