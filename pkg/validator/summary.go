51d639e2ef0354152ee43bb990c3d5ff2d28ba95
Bader Boland <bader@fairwinds.com>
2020-01-31 12:56:53 -0500

    Prevent divide by 0

--- pkg/validator/summary.go
+++ pkg/validator/summary.go
@@ -17,6 +17,9 @@ type CountSummaryByCategory map[string]CountSummary
 // GetScore returns an overall score in [0, 100] for the CountSummary
 func (cs CountSummary) GetScore() uint {
 	total := (cs.Successes * 2) + cs.Warnings + (cs.Errors * 2)
+	if total == 0 {
+		total = 1 // Prevent divide by 0.
+	}
 	return uint((float64(cs.Successes*2) / float64(total)) * 100)
 }
 
