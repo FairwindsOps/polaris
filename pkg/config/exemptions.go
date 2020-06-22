0a0720a26ccb59e9098d4c657e5d3129ec1741a4
hgoscenski-vail <49690293+hgoscenski-vail@users.noreply.github.com>
2020-06-22 13:18:23 -0500

    Adds option to exempt an entire controller from checks via config file (#350)
    This adds the ability to exempt a controller from all checks similar to
    the annotation for "exempt" which exempts all checks.
    
    I added the tests to go with this as well as for the IsActionable
    function.

--- pkg/config/exemptions.go
+++ pkg/config/exemptions.go
@@ -12,11 +12,19 @@ func (conf Configuration) IsActionable(ruleID, controllerName string) bool {
 	if conf.DisallowExemptions {
 		return true
 	}
+
 	for _, example := range conf.Exemptions {
 		for _, rule := range example.Rules {
 			if rule != ruleID {
 				continue
 			}
+			for _, controller := range example.ControllerNames {
+				if strings.HasPrefix(controllerName, controller) {
+					return false
+				}
+			}
+		}
+		if len(example.Rules) == 0 {
 			for _, controller := range example.ControllerNames {
 				if strings.HasPrefix(controllerName, controller) {
 					return false
