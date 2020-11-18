b0c9a0a40744c250bd2d28c52b34a57de2be2123
Robert Brennan <contact@rbren.io>
2020-11-18 22:08:51 +0000

    build docs site

---
+++ docs/scripts/modify.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){
+  setTimeout(function() {
+    var link = document.getElementsByClassName('home-link')[0];
+    linkClone = link.cloneNode(true);
+    linkClone.href = "https://fairwinds.com";
+    link.setAttribute('target', '_blank');
+    link.parentNode.replaceChild(linkClone, link);
+  }, 1000);
+});
+
