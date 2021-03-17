3b955e611c342d819042360561d3116289fa7794
Robert Brennan <accounts@rbren.io>
2021-03-17 13:55:36 -0400

    Update documentation from template (#518)

--- docs/.vuepress/public/scripts/marketing.js
+++ docs/.vuepress/public/scripts/marketing.js
@@ -26,4 +26,4 @@ fbq('track', 'PageView');
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-K5KK5H3');
+})(window,document,'script','dataLayer','GTM-TM95WXQ');
