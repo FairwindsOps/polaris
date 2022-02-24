d410278ac1b6a87980faa989beb6ffe0bd7355c5
MAKOSCAFEE <barnabasmakonda@gmail.com>
2022-02-24 16:46:37 +0200

    revert resolve export

--- pkg/validator/schema.go
+++ pkg/validator/schema.go
@@ -22,7 +22,7 @@ type schemaTestCase struct {
 	ResourceProvider *kube.ResourceProvider
 }
 
-func ResolveCheck(conf *config.Configuration, checkID string, test schemaTestCase) (*config.SchemaCheck, error) {
+func resolveCheck(conf *config.Configuration, checkID string, test schemaTestCase) (*config.SchemaCheck, error) {
 	if !conf.DisallowExemptions &&
 		!conf.DisallowAnnotationExemptions &&
 		hasExemptionAnnotation(test.Resource.ObjectMeta, checkID) {
@@ -231,7 +231,7 @@ func applySchemaChecks(conf *config.Configuration, test schemaTestCase) (ResultS
 }
 
 func applySchemaCheck(conf *config.Configuration, checkID string, test schemaTestCase) (*ResultMessage, error) {
-	check, err := ResolveCheck(conf, checkID, test)
+	check, err := resolveCheck(conf, checkID, test)
 	if err != nil {
 		return nil, err
 	} else if check == nil {
