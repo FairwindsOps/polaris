52e3290cfb2a46be4409c346518f96079e8608b6
Robert Brennan <contact@rbren.io>
2022-09-15 11:49:49 -0400

    refactor test

--- pkg/mutation/mutate_test.go
+++ pkg/mutation/mutate_test.go
@@ -19,85 +19,81 @@ pets:
   - name: scooby
 `
 
-var nonOwnersKeyYaml = `
-pets:
-  - name: fido
-`
-
-var locationYaml = `pets:
-  - name: fido
-    owners:
-      - name: Alice
-        location: Denver
-      - name: Bob
-        aliases:
-          - Robert
-        location: Denver
-  - name: scooby
-`
-
-var redactedYaml = `pets:
-  - name: fido
-    owners:
-      - name: Alice
-      - name: Bob
-  - name: scooby
-`
-var aliasesYaml = `pets:
-  - name: fido
-    owners:
-      - name: Alice
-        aliases:
-          - rob
-      - name: Bob
-        aliases:
-          - Robert
-          - rob
-  - name: scooby
-`
-
-var addedOwnersKeyYaml = `pets:
-  - name: fido
-    owners:
-      - name: Alice
-`
-
-func TestApplyAllMutations(t *testing.T) {
-
-	mutation := config.Mutation{
+var testCases = []struct{
+	original string
+	mutated string
+	patch config.Mutation
+}{{
+	original: oldYaml,
+	patch: config.Mutation{
 		Op:    "add",
 		Value: "Denver",
 		Path:  "/pets/0/owners/*/location",
-	}
-
-	mutated, err := ApplyAllMutations(oldYaml, []config.Mutation{mutation})
-	assert.NoError(t, err)
-	assert.EqualValues(t, locationYaml, mutated)
-
-	mutation = config.Mutation{
+	},
+	mutated: `pets:
+  - name: fido
+    owners:
+      - name: Alice
+        location: Denver
+      - name: Bob
+        aliases:
+          - Robert
+        location: Denver
+  - name: scooby
+`,
+}, {
+	original: oldYaml,
+	patch: config.Mutation{
 		Op:   "remove",
 		Path: "/pets/0/owners/*/aliases",
-	}
-
-	mutated, err = ApplyAllMutations(oldYaml, []config.Mutation{mutation})
-	assert.NoError(t, err)
-	assert.EqualValues(t, redactedYaml, mutated)
-
-	mutation = config.Mutation{
+	},
+	mutated: `pets:
+  - name: fido
+    owners:
+      - name: Alice
+      - name: Bob
+  - name: scooby
+`,
+}, {
+	original: oldYaml,
+	patch: config.Mutation{
 		Op:    "add",
 		Value: "rob",
 		Path:  "/pets/0/owners/*/aliases/-",
-	}
-	mutated, err = ApplyAllMutations(oldYaml, []config.Mutation{mutation})
-	assert.NoError(t, err)
-	assert.EqualValues(t, aliasesYaml, mutated)
-
-	mutation = config.Mutation{
+	},
+	mutated: `pets:
+  - name: fido
+    owners:
+      - name: Alice
+        aliases:
+          - rob
+      - name: Bob
+        aliases:
+          - Robert
+          - rob
+  - name: scooby
+`,
+}, {
+	original: `
+pets:
+  - name: fido
+`,
+	patch: config.Mutation{
 		Op:    "add",
 		Value: "Alice",
 		Path:  "/pets/0/owners/0/name",
+	},
+	mutated: `pets:
+  - name: fido
+    owners:
+      - name: Alice
+`,
+}}
+
+func TestApplyAllMutations(t *testing.T) {
+	for _, tc := range testCases {
+		mutated, err := ApplyAllMutations(tc.original, []config.Mutation{tc.patch})
+		assert.NoError(t, err)
+		assert.EqualValues(t, tc.mutated, mutated)
 	}
-	mutated, err = ApplyAllMutations(nonOwnersKeyYaml, []config.Mutation{mutation})
-	assert.NoError(t, err)
-	assert.EqualValues(t, addedOwnersKeyYaml, mutated)
 }
