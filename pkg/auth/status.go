a5ada4303bdd562d436c162b6b09e94e9b220349
Vitor Vezani <vitor.vezani@fairwinds.com>
2023-05-16 10:38:05 -0300

    implement status command

--- pkg/auth/status.go
+++ pkg/auth/status.go
@@ -1,24 +1,35 @@
 package auth
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func PrintStatus() error {
-
 	if content, err := readPolarisHostsFile(); err == nil {
 		if len(content) > 0 {
 			for k, h := range content {
-				fmt.Println(k, h)
-				// TODO: Vitor - implement this
-				/*
-					✓ Logged in to github.com as vitorvezani (keyring)
-					✓ Git operations for github.com configured to use ssh protocol.
-					✓ Token: ghp_************************************
-					✓ Token scopes: admin:public_key, read:org, repo
-				*/
+				if len(content) == 1 {
+					fmt.Printf("✓ Logged in to %s as <user>\n", k)
+					fmt.Printf("✓ Token: %s\n", hideToken(h.Token, 3))
+				}
 			}
 			return nil
 		}
 	}
 	fmt.Println("You are not logged into Fairwinds Insights. Run polaris auth login to authenticate.")
 	return nil
+}
+
+func hideToken(token string, hideAfter int) string {
+	var i int
+	return strings.Map(func(r rune) rune {
+		defer func() {
+			i++
+		}()
+		if i > hideAfter {
+			return []rune("*")[0]
+		}
+		return r
+	}, token)
 }
