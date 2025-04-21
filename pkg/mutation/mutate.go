7a718a9b95463f912e8def70b7b62b19271ea38b
jdesouza <james@fairwinds.com>
2025-04-21 11:41:34 -0300

    Debugging

--- pkg/mutation/mutate.go
+++ pkg/mutation/mutate.go
@@ -166,6 +166,7 @@ func createPathAndFindNodes(node *yaml.Node, selectors []string, create bool) ([
 	case yaml.SequenceNode:
 		//return nil, errors.Errorf("parent node is array, use /*/ or /0/../%v/ instead of .%v to access its item(s) first", len(node.Content)-1, currentSelector)
 
+		break
 	default:
 		return nil, errors.Errorf("parent node is of unknown kind %v", node.Kind)
 	}
