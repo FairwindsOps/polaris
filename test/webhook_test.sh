0444cf92bce91eadac360c328dc7573c50df1c48
Vitor Rodrigo Vezani <vitor.vezani@fairwinds.com>
2026-04-27 12:24:11 -0300

    unify release process along with others OSS projects (#1192)
    * unify release process along with others OSS projects
    
    * remove docker loging
    
    * override polaris image repository for tests
    
    * fix polaris
    
    * adding snapshot back
    
    * store snapshot version into circleci workspace and load with kind on k8s cluster
    
    * add test back
    
    * add test back
    
    * fix polaris config

--- test/webhook_test.sh
+++ test/webhook_test.sh
@@ -88,7 +88,9 @@ helm install polaris fairwinds-stable/polaris --namespace polaris --create-names
   --set dashboard.enable=false \
   --set webhook.enable=true \
   --set webhook.mutate=true \
-  --set image.tag=$CI_SHA1
+  --set image.repository=us-docker.pkg.dev/fairwinds-ops/oss/polaris \
+  --set image.tag=$CI_SHA1 \
+  --set image.pullPolicy=IfNotPresent
 
 echo "Waiting for the webhook to come online"
 check_webhook_is_ready
